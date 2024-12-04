fa7b3d6037374ceaad0f0a8e88c936665bff17c5
David Poole <hymnally.yours@gmail.com>
2024-12-04 12:56:06 -0500

    Stop Editor GUI setup

---
+++ src/gui/stop_editor/pipeeditor.py
@@ -0,0 +1,24 @@
+"""Pipe Editor"""
+
+
+from PySide6.QtWidgets import (
+    QFrame
+)
+from PySide6.QtGui import Qt
+
+
+class PipeEditor(QFrame):
+    def __init__(self):
+        super().__init__()
+        self.__init_ui()
+        self.__ui_settings()
+        self.__ui_layout()
+
+    def __init_ui(self):
+        ...
+
+    def __ui_settings(self):
+        ...
+
+    def __ui_layout(self):
+        ...
---
+++ src/gui/stop_editor/stopinfo.py
@@ -0,0 +1,24 @@
+"""Stop Info"""
+
+
+from PySide6.QtWidgets import (
+    QGroupBox
+)
+from PySide6.QtGui import Qt
+
+
+class StopInfo(QGroupBox):
+    def __init__(self):
+        super().__init__()
+        self.__init_ui()
+        self.__ui_settings()
+        self.__ui_layout()
+
+    def __init_ui(self):
+        ...
+
+    def __ui_settings(self):
+        ...
+
+    def __ui_layout(self):
+        ...
